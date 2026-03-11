912dc574e45dfde8a58a21aa7121c53b9ee0db76
walid <walidrahman.officials@gmail.com>
2026-03-11 12:08:23 +0600

    added

---
+++ basics4.dart
@@ -0,0 +1,19 @@
+void printName() {
+  print("walid");
+}
+
+String printString() {
+  return "it's a string";
+}
+
+int printInt() {
+  return 18;
+}
+
+void main() {
+  //functions - <datatype> functionName(){}
+
+  printName();
+  print(printString());
+  print(printInt());
+}
